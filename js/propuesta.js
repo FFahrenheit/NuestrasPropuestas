da4ec1fa03a6e1b10e7b7c4b497b351a35096c6f
Iván L. Murillo <diskman199@gmail.com>
2021-10-17 19:04:21 -0500

    Propuesta generada

---
+++ js/propuesta.js
@@ -0,0 +1,44 @@
+const propuesta = {
+    titulo: document.getElementById('p-titulo'),
+    contenido: document.getElementById('p-contenido'),
+    fecha: document.getElementById('p-fecha'),
+    partido: document.getElementById('p-partido'),
+    autor: document.getElementById('p-autor'),
+    link: document.getElementById('p-descarga')
+}
+
+window.onload = () =>{
+    const queryString = window.location.search;
+    console.log(queryString);
+
+    if(queryString == ''){
+        window.location.href = 'busqueda.html'
+    }else{
+        const urlParams = new URLSearchParams(queryString);
+        let id = urlParams.get('id');
+        loadInfo(id);
+    }
+}
+
+function loadInfo(id){
+    fetch('js/tests/propuestas.json')
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data);
+        renderInfo(data[id - 1]); //La posición id - 1 
+    })
+    .catch(error=>{
+        console.log(error);
+    });
+}
+
+function renderInfo(data){
+    console.log(data);
+    propuesta.titulo.innerHTML = data.titulo;
+    propuesta.contenido.innerHTML = data.contenido;
+    propuesta.partido.innerHTML = data.partido;
+    propuesta.fecha.innerHTML = data.fecha;
+    propuesta.autor.innerHTML = data.autor;
+    propuesta.link.href = 'assets/tests/propuesta-test.pdf'
+    propuesta.link.target = '_blank';
+}
