d035621af10e2702f781fdef14c862531667b18f
Iván L. Murillo <diskman199@gmail.com>
2021-11-07 21:19:03 -0600

    Dashboard de propuestas

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,59 +1,87 @@
 
-// const searchTitle = document.getElementById('search-title');
-// const propuestas = document.getElementById('propuestas');
+const searchTitle = document.getElementById('search-title');
+const propuestas = document.getElementById('propuestas');
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
 
-// window.onload = () =>{
-//     const queryString = window.location.search;
-//     console.log(queryString);
+searchButton.addEventListener('click', handleSearch);
+searchInput.addEventListener('submit', handleSearch);
 
-//     if(queryString == ''){
-//         searchTitle.innerHTML = 'Propuestas del congreso: ';
-//         searchWithKeywords('');
-//     }else{
+function handleSearch(e){
+    e.preventDefault();
+    let searchQuery = searchInput.value;
+    console.log(encodeURIComponent(searchQuery));
+    if(searchQuery.length > 0){
+        searchQuery = encodeURIComponent(searchQuery);
+        window.location.href = 'dashboard.html?busqueda=' + searchQuery;
+    }else{
+        window.location.href = 'dashboard.html';
+    }
+}
 
-//         const urlParams = new URLSearchParams(queryString);
-//         let keywords = urlParams.get('busqueda');
+window.onload = () =>{
+    const queryString = window.location.search;
+    console.log(queryString);
 
-//         searchTitle.innerHTML = `Búsqueda de propuestas con respecto a "${ keywords }": `;
+    if(queryString == ''){
+        searchTitle.innerHTML = 'Propuestas del congreso: ';
+        searchWithKeywords('');
+    }else{
+
+        const urlParams = new URLSearchParams(queryString);
+        let keywords = urlParams.get('busqueda');
+
+        searchTitle.innerHTML = `Búsqueda de propuestas con respecto a "${ keywords }": `;
         
-//         searchInput.value = keywords;
-//         searchWithKeywords(keywords);
-//     }
-// }
+        searchInput.value = keywords;
+        searchWithKeywords(keywords);
+    }
+}
 
-// function searchWithKeywords(keywords){
-//     console.log(keywords);
-//     fetch('js/tests/propuestas.json')
-//     .then(res => res.json())
-//     .then(data =>{
-//         console.log(data);
-//         generateCards(data);
-//     })
-//     .catch(error=>{
-//         console.log(error);
-//     });
-// }
+function searchWithKeywords(keywords){
+    console.log(keywords);
+    fetch('../js/tests/propuestas.json')
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data);
+        generateCards(data);
+    })
+    .catch(error=>{
+        console.log(error);
+    });
+}
 
-// function generateCards(data){
-//     propuestas.innerHTML = '';
-//     data.forEach(d =>{
-//         let html = `
-//         <div class="card m-3">
-//             <div class="card-body">
-//                 <h5 class="card-title">${ d.titulo }</h5>
-//                 <h6 class="card-subtitle text-muted">
-//                     <i class="fas fa-user-alt mx-3"></i> ${ d.autor }
-//                     <i class="fas fa-clock mx-3"></i> ${ d.fecha }
-//                     <i class="fas fa-vote-yea mx-3"></i> ${ d.partido }
-//                 </h6>
-//                 <p class="card-text my-1">
-//                     ${ d.contenido }
-//                 </p>
-//                 <a href="propuesta.html?id=${ d.id }" class="card-link text-danger">
-//                     Ver detalles
-//                 </a>
-//             </div>
-//         </div>`;
-//         propuestas.innerHTML += html;
-//     });
-// }
+function generateCards(data){
+    propuestas.innerHTML = '';
+    data.forEach(d =>{
+        let html = `
+        <div class="card m-3">
+            <div class="card-body">
+                <h5 class="card-title">${ d.titulo }</h5>
+                <h6 class="card-subtitle text-muted">
+                    <i class="fas fa-user-alt mx-3"></i> ${ d.autor }
+                    <i class="fas fa-clock mx-3"></i> ${ d.fecha }
+                    <i class="fas fa-vote-yea mx-3"></i> ${ d.partido }
+                </h6>
+                <p class="card-text my-1">
+                    ${ d.contenido }
+                </p>
+                <a href="/admin/nueva.html?id=${ d.id }" class="btn btn-primary card-link">
+                    Modificar detalles
+                </a>
+                <a onclick="deletePropuesta(${ d.id })" class="btn btn-danger card-link text-light" style="cursor:pointer;">
+                    Eliminar propuesta
+                </a>
+            </div>
+        </div>`;
+        propuestas.innerHTML += html;
+    });
+}
+
+function deletePropuesta(id){
+    if(confirm('¿Desea eliminar la propuesta con ID ' + id +'?')){
+        console.log('Eliminado');
+    }else{
+        console.log('No eliminado');
+    }
+}
