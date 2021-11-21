683de2ab4192b1e00bdb4ebac07737a670f5de13
Iván L. Murillo <diskman199@gmail.com>
2021-11-21 15:18:14 -0600

    Conectado a la API para leer el archivo

--- js/nueva.js
+++ js/nueva.js
@@ -1,5 +1,7 @@
 const title = document.getElementById('h-titulo');
 const form = document.getElementById('form');
+const archivo = document.getElementById('archivo');
+const fileStatus = document.getElementById('file_status');
 
 const propuesta = {
     titulo: document.getElementById('titulo'),
@@ -14,33 +16,33 @@ window.onload = () => {
     const queryString = window.location.search;
     console.log(queryString);
 
-    if(queryString == ''){
+    if (queryString == '') {
         title.innerHTML = 'Subir propuesta';
-    }else{
+    } else {
 
         const urlParams = new URLSearchParams(queryString);
         let id = urlParams.get('id');
 
-        title.innerHTML = `Editar propuesta #${ id }: `;        
+        title.innerHTML = `Editar propuesta #${id}: `;
 
         loadInfo(id);
     }
 }
 
 
-function loadInfo(id){
+function loadInfo(id) {
     fetch('../js/tests/propuestas.json')
-    .then(res => res.json())
-    .then(data =>{
-        console.log(data);
-        renderInfo(data[id - 1]); //La posición id - 1 
-    })
-    .catch(error=>{
-        console.log(error);
-    });
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            renderInfo(data[id - 1]); //La posición id - 1 
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
 
-function renderInfo(data){
+function renderInfo(data) {
     console.log(data);
     console.log(propuesta.titulo);
     propuesta.titulo.value = data.titulo;
@@ -54,7 +56,41 @@ function renderInfo(data){
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    if(confirm('¿Desea guardar los cambios hechos?')){
+    if (confirm('¿Desea guardar los cambios hechos?')) {
         console.log('Ok!');
     }
-})
+});
+
+archivo.addEventListener('change', () => {
+    const file = archivo.files[0];
+    let data = new FormData();
+    data.append('files[]', file);
+
+    fileStatus.innerHTML = `
+    <div class="spinner-border text-success loading" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>`;
+
+    fetch('http://127.0.0.1:5000/api/v0/admin/files', {
+        method: 'POST',
+        body: data
+    }).then(res => res.json())
+        .then(resp => {
+            console.log(resp);
+            if (resp['code'] == 'ok') {
+                fileStatus.innerHTML = `<i class="fas fa-check-circle text-success"></i>`;
+                form.contenido.value = resp['extract'] || 'N/A';
+                form.contenido.scrollTop = form.contenido.scrollHeight;
+
+            } else if (resp['code'] == 'fail') {
+                alert('Lectura de archivo no completada');
+                fileStatus.innerHTML = `<i class="fas fa-exclamation-circle text-danger"></i>`;
+            } else {
+                alert('Error en el servidor');
+                fileStatus.innerHTML = `<i class="fas fa-exclamation-circle text-danger"></i>`;
+            }
+        }).catch(error => {
+            fileStatus.innerHTML = `<i class="fas fa-exclamation-circle"></i>`;
+            alert('No se pudo establecer conexión al servidor');
+        })
+});
