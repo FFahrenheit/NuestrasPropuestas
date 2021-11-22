ed552a2820c5bc5dfeb4c0a1f0f60fe8eb587324
Iván L. Murillo <diskman199@gmail.com>
2021-11-22 09:52:45 -0600

    Feedback recibiendo!

--- js/contacto.js
+++ js/contacto.js
@@ -1,6 +1,31 @@
 const form = document.getElementById('contact-form');
+const message = document.getElementById('message');
+const nombre = document.getElementById('nombre');
+const email = document.getElementById('email');
+
 
 form.onsubmit = (e) =>{
     e.preventDefault();
-    console.log('To be implemented!');
+    const data = {
+        message: `${ message.value }\n\n-${ nombre.value }`,
+        email: email.value
+    };
+    const body = JSON.stringify(data);
+
+    console.log({body, data, target: e.target.action});  
+
+    fetch(e.target.action,{
+        method: 'POST',
+        body: body,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(resp=>{
+        console.log(resp);
+        form.reset();
+        alert('¡Feedback recibido!')
+    }).catch(error=>{
+        console.log(error);
+        alert('No pudimos enviar el feedback');
+    });
 }
